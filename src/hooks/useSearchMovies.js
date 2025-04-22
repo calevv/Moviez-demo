2b0990b11de46fc50273abcad79481186ebe168e
jeong-minG <ika818488@gmail.com>
2025-04-22 23:03:25 +0900

    페이지네이션&검색결과알럿

--- src/hooks/useSearchMovies.js
+++ src/hooks/useSearchMovies.js
@@ -1,16 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import api from "../utils/api";
+import { useQuery } from '@tanstack/react-query';
+import api from '../utils/api';
 
-const fetchSearch = ({ keyword }) => {
-  return keyword
-    ? api.get(`/search/movie?query=${keyword}&language=ko-KR`)
-    : api.get(`/movie/popular?language=ko-KR`);
+const fetchSearch = ({ keyword, page }) => {
+    return keyword
+        ? api.get(`/search/movie?query=${keyword}&page=${page}&language=ko-KR`)
+        : api.get(`/movie/popular?language=ko-KR&page=${page}`);
 };
 
-export const useSearchMoviesQuery = ({ keyword }) => {
-  return useQuery({
-    queryKey: ["movie-search", keyword],
-    queryFn: () => fetchSearch({ keyword }),
-    select: (reault) => reault.data,
-  });
+export const useSearchMoviesQuery = ({ keyword, page }) => {
+    return useQuery({
+        queryKey: ['movie-search', keyword, page],
+        queryFn: () => fetchSearch({ keyword, page }),
+        select: (reault) => reault.data,
+    });
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div>
             <Banner />
-            <Container style={{ padding: '20px', display: 'flex', flexDirection: 'column', gap: '40px' }}>
+            <Container style={{ padding: '40px 20px', display: 'flex', flexDirection: 'column', gap: '40px' }}>
                 <PopularMoviesSlide />
                 <UpcommingMoviesSlide />
                 <NowMoviesSlide />
